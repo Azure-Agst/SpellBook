d2a059ba3e0f9c6ec8637f99aa941ed76a678df9
Azure-Agst <August712@protonmail.com>
2018-09-30 22:01:31 -0400

    added letsencrypt

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 ///////////////////////////////
 
 // Module Imports
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 const express = require('express');
 const session = require('express-session');
 const app = express();
@@ -12,6 +15,16 @@ const path = require("path");
 
 // init static dirs
 app.use('/public', express.static(path.join(__dirname, '/public'))); // set ./assets as static folder
+
+// security may be a bit important.
+app.use(function(req, res, next) {
+  if(!req.secure) {
+    var secureUrl = "https://" + req.headers['host'] + req.url;
+    res.writeHead(301, { "Location":  secureUrl });
+    res.end();
+  }
+  next();
+});
 
 // init API
 app.use(session({secret:"TotallySecret", cookie:{}}));
@@ -29,6 +42,30 @@ app.get("*", function(req, res){
 })
 
 // Finish and Listen
-app.listen(80, function(){
-  console.log("[Server] Server Started!");
-})
+if (process.env.NODE_ENV == 'production'){
+  const privateKey = fs.readFileSync('/etc/letsencrypt/live/spellbook.azureagst.com/privkey.pem', 'utf8');
+  const certificate = fs.readFileSync('/etc/letsencrypt/live/spellbook.azureagst.com/cert.pem', 'utf8');
+  const ca = fs.readFileSync('/etc/letsencrypt/live/spellbook.azureagst.com/chain.pem', 'utf8');
+
+  const credentials = {
+  	key: privateKey,
+  	cert: certificate,
+  	ca: ca
+  };
+
+  const httpServer = http.createServer(app);
+  const httpsServer = https.createServer(credentials, app);
+
+  httpServer.listen(80, () => {
+  	console.log('HTTP Server running on port 80');
+  });
+
+  httpsServer.listen(443, () => {
+  	console.log('HTTPS Server running on port 443');
+  });
+
+} else {
+  app.listen(443, function(){
+    console.log("[Server] Server Started!");
+  })
+}
